05d7d4d060e75145e2c654152c605030d4e19964
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-16 16:36:38 -0700

    catsgan

--- cats-GAN/generator.py
+++ cats-GAN/generator.py
@@ -1,1 +1,31 @@
+import tensorflow as tf
 from tensorflow.keras import layers
+
+class Generator:
+
+    def __init__(self):
+        self.generator = self.__make_generator__()
+    def __make_generator__(self):
+
+        model = tf.keras.Sequential()
+        model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
+        model.add(layers.BatchNormalization())
+        model.add(layers.LeakyReLU())
+
+        model.add(layers.Reshape((7, 7, 256)))
+
+        model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), use_bias=False, padding='same'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.LeakyReLU())
+
+        model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), use_bias=False, padding='same'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.LeakyReLU())
+
+        model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), use_bias=False, padding='same'))
+        assert model.output_shape == (None, 28, 28, 1)
+
+        return model
+
+    def __generator_loss__(self):
+        return None
