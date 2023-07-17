e3197eae7107f0565f4b53d9371beca04cf7d2a4
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-16 18:59:58 -0700

    arc updates

--- cats-GAN/generator.py
+++ cats-GAN/generator.py
@@ -8,11 +8,11 @@ class Generator:
     def __make_generator__(self):
 
         model = tf.keras.Sequential()
-        model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
+        model.add(layers.Dense(16 * 16 * 256, use_bias=False, input_shape=(100,)))
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Reshape((7, 7, 256)))
+        model.add(layers.Reshape((16, 16, 256)))
 
         model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), use_bias=False, padding='same'))
         model.add(layers.BatchNormalization())
@@ -23,7 +23,7 @@ class Generator:
         model.add(layers.LeakyReLU())
 
         model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), use_bias=False, padding='same'))
-        assert model.output_shape == (None, 28, 28, 1)
+        assert model.output_shape == (None, 64, 64, 1)
 
         return model
 
