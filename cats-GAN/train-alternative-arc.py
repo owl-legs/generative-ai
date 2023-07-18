e5a9cc4862166bbe34ebe5832949a6ba1475942d
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-17 18:22:54 -0700

    alternative training file and shell script

---
+++ cats-GAN/train-alternative-arc.py
@@ -0,0 +1,44 @@
+import os
+import time
+import config
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from image_parser import ImageParser
+from discriminator import Discriminator
+from generator import Generator
+
+from IPython import display
+
+BATCH_SIZE = config.BATCH_SIZE
+
+print("opening and processing all the cat faces.")
+
+parser = ImageParser()
+train_images = parser.create_batches()
+
+disc, gen = Discriminator().discriminator, Generator().generator
+
+def def_gan(g, d):
+
+    d.trainable = False
+
+    model = tf.keras.Sequential(name='DCGAN-V2')
+    model.add(g)
+    model.add(d)
+
+    model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002, beta_1=0.5))
+
+    return model
+
+gan_model = def_gan(gen, disc)
+gan_model.summary()
+
+def train(gen, disc, dataset, latent_dim):
+
+    for epoch in range(config.EPOCHS):
+
+        pass
+
+
+
